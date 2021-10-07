1025983d3172736885ef91b8ea14140608bd1e5e
GroundP <bloom0819@naver.com>
2021-10-08 01:03:27 +0900

    complete entities

--- src/entities/Sleact.ts
+++
@@ -1,7 +0,0 @@
-import { Column, Entity } from "typeorm";
-
-@Entity("sleact", { schema: "nestDB" })
-export class Sleact {
-  @Column("int", { primary: true, name: "idsleact" })
-  idsleact: number;
-}

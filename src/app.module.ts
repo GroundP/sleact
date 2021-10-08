8eb2339ed46048b1175c3b45cf96f4bb66e9e90d
GroundP <bloom0819@naver.com>
2021-10-09 02:28:16 +0900

    install the typeORM, add option to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,11 @@
+import { Workspaces } from './entities/Workspaces';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Users } from './entities/Users';
+import { Mentions } from './entities/Mentions';
+import { DMs } from './entities/DMs';
+import { Channels } from './entities/Channels';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { ChannelChats } from './entities/ChannelChats';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +13,46 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from '../ormconfig';
 
 @Module({
-  imports: [UsersModule, WorkspacesModule, DmsModule, ChannelsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    WorkspacesModule,
+    DmsModule,
+    ChannelsModule,
+    //TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: 'admin',
+      password: process.env.MYSQL_PASSWORD,
+      database: 'nestDB',
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      migrations: [__dirname + '/src/migrations/*.ts'],
+      cli: { migrationsDir: 'src/migrations' },
+      autoLoadEntities: true,
+      charset: 'utf8mb4',
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
